c1c04a573ac7bc00fc4db4b5b897bb6ff9fa5507
Pat Allan <pat@freelancing-gods.com>
2010-05-14 17:40:57 +1000

    Let's try to get fish and rvm playing nicely

--- rvm.rb
+++ rvm.rb
@@ -1,7 +1,8 @@
 dep 'rvm' do
-  helper :path do
+  helper :path {
     '~/.rvm/scripts/rvm'
-  end
+  }
+  
   met? {
     path.p.file?
   }
@@ -14,5 +15,34 @@ dep 'rvm' do
     # Make sure RVM is always loaded by bash
     profile = Dir["~/.bash_profile", "~/.bashrc"].first || "~/.bash_profile"
     shell "echo 'if [[ -s \"$HOME/.rvm/scripts/rvm\" ]]  ; then source \"$HOME/.rvm/scripts/rvm\" ; fi' >> #{profile}"
+  }
+end
+
+dep 'fish rvm' do
+  requires 'fish', 'rvm'
+  
+  helper :github {
+    'http://github.com/eventualbuddha/fish-nuggets/raw/master/functions'
+  }
+  helper :functions {
+    '~/.config/fish/functions'
+  }
+  helper :rvm_env {
+    functions / '__bash_env_to_fish.fish'
+  }
+  helper :rvm_fish {
+    functions / 'rvm.fish'
+  }
+  helper :cd_fish {
+    functions / 'cd.fish'
+  }
+  
+  met? {
+    which rvm_env
+  }
+  meet {
+    shell "curl --create-dirs -o #{rvm_env} #{github}/__bash_env_to_fish.fish"
+    shell "curl -o #{rvm_fish} #{github}/rvm.fish"
+    shell "curl -o #{cd_fish} #{github}/cd.fish"
   }
 end
