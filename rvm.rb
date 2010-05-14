4187af4f1e2c26842db0548334177ad12b1efd6c
Pat Allan <pat@freelancing-gods.com>
2010-05-14 17:31:05 +1000

    Can't detect rvm - need to avoid script for the moment

--- rvm.rb
+++ rvm.rb
@@ -1,13 +1,15 @@
 dep 'rvm' do
+  helper :path do
+    '~/.rvm/scripts/rvm'
+  end
   met? {
-    which 'rvm'
+    path.p.file?
   }
   meet {
     # Install from Github using provided script
     shell 'bash -c "`curl http://rvm.beginrescueend.com/releases/rvm-install-head`"'
     
-    shell 'bash -c "source ~/.rvm/scripts/rvm"'
-    shell 'bash -c "rvm notes"'
+    shell 'bash -c "source ~/.rvm/scripts/rvm; rvm notes"'
     
     # Make sure RVM is always loaded by bash
     profile = Dir["~/.bash_profile", "~/.bashrc"].first || "~/.bash_profile"
