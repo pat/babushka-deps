907872127b41d3ad4c20ed0c025ee8dbaddd0c93
Pat Allan <pat@freelancing-gods.com>
2010-05-18 16:42:44 +1000

    Cleaning things up and making sure post-install requirements are taken care of as well.

--- mysql.rb
+++ mysql.rb
@@ -4,4 +4,20 @@ pkg 'mysql software' do
     via :brew, 'mysql'
   }
   provides 'mysql'
+  
+  helper(:plist) {
+    'com.mysql.mysqld.plist'
+  }
+  helper(:brew_path) {
+    BrewHelper.brew_path_for('mysql')
+  }
+  helper(:launch_agents) {
+    '~/Library/LaunchAgents'
+  }
+  
+  after(:on => :osx) {
+    shell 'mysql_install_db'
+    shell "cp #{brew_path}/#{plist} #{launch_agents}"
+    shell "launchctl load -w #{launch_agents}/#{plist}"
+  }
 end
